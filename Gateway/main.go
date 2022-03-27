cb578acffd2b7317d2e4667b8984ca6c5f5fed30
emon100 <emon100@qq.com>
2022-03-28 03:10:45 +0800

    feat: add putId.

--- Gateway/main.go
+++ Gateway/main.go
@@ -1,47 +1,75 @@
 package main
 
 import (
+	connector "HADSS/StorageConnector"
 	"crypto/sha256"
 	"encoding/hex"
 	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"io/ioutil"
 	"log"
+	"strings"
 )
 
-func parseArguments() (addr string, port int) {
-	flag.StringVar(&addr, "addr", "0.0.0.0", "The IP address to listen.")
-	flag.IntVar(&port, "port", 10900, "The port to listen.")
+func parseArguments() (addr string) {
+	flag.StringVar(&addr, "addr", "localhost:9090", "The address to connect.")
 	flag.Parse()
+	addr = strings.TrimRight(addr, "/")
 	return
 }
 
 var addr string
-var port int
 
 func main() {
-	addr, port = parseArguments()
-	log.Println(addr, port)
-
-	h := sha256.New()
-	h.Write([]byte("try"))
-	sha := h.Sum(nil)
-	log.Printf("SHA-256: %s", hex.EncodeToString(sha))
+	addr = parseArguments()
 
 	r := gin.Default()
 	r.GET("/id/:id", getId)
+	r.PUT("/id/:id", putId)
 
-	err := r.Run(fmt.Sprintf("%s:%d", addr, port))
+	err := r.Run(addr)
 	if err != nil {
 		fmt.Println(err)
 	}
 
 }
 
+func get_storage_node_addr() string { //TODO
+	return "http://localhost:10001"
+}
+
 func getId(c *gin.Context) {
 	id := c.Query("id")
-	h := sha256.New()
-	h.Write([]byte(id))
-	sha := h.Sum(nil)
-	log.Printf("SHA-256: %s", hex.EncodeToString(sha))
+	h := sha256.Sum256([]byte(id))
+	log.Printf("getId: SHA-256: %s", hex.EncodeToString(h[:]))
+
+	conn := connector.NewBasicConnection(get_storage_node_addr(), connector.StrongConsistency)
+	data, err := conn.GetSlice(h[:])
+	if err != nil {
+		c.AbortWithError(502, err)
+		return
+	}
+	c.Data(200, "application/octet-stream", data)
+}
+
+func putId(c *gin.Context) {
+	id := c.Query("id")
+	h := sha256.Sum256([]byte(id))
+	log.Printf("putId: SHA-256: %s", hex.EncodeToString(h[:]))
+
+	data, err := ioutil.ReadAll(c.Request.Body) //TODO: Slice the file.
+	defer c.Request.Body.Close()
+	if err != nil {
+		c.AbortWithError(502, fmt.Errorf("Server create slice fail. Detail: %w", err))
+		return
+	}
+
+	conn := connector.NewBasicConnection(get_storage_node_addr(), connector.StrongConsistency)
+	err = conn.PutSlice(h[:], data)
+	if err != nil {
+		c.AbortWithError(502, err)
+		return
+	}
+	c.Data(200, "application/octet-stream", []byte("Upload finished."))
 }
