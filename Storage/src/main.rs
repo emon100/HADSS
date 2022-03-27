4229f0e02b7f0f4d536c529d9b5ae681be8864a6
emon100 <emon100@qq.com>
2022-03-28 03:11:10 +0800

    feat: cli arguments parser for storage nodes.

--- Storage/src/main.rs
+++ Storage/src/main.rs
@@ -1,5 +1,32 @@
+#![feature(once_cell)]
+#![feature(iter_intersperse)]
+mod server;
 
+use clap::Parser;
+use server::init_httpserver;
+use std::lazy::SyncLazy;
 
-fn main() {
-    println!("Hello, world!");
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    #[clap(short, long, default_value = "0.0.0.0")]
+    addr: String,
+    #[clap(short, long, default_value_t = 10001)]
+    port: u16,
+    #[clap(short, long, default_value = "/tmp/storage")]
+    storage_location: String,
+    #[clap(long, default_value_t = 10)]
+    storage_directory_depth: usize,
+}
+
+pub static ARGS: SyncLazy<Args> = SyncLazy::new(|| {
+    let mut args: Args = Args::parse();
+    args.storage_location = args.storage_location.trim_end_matches("/").parse().unwrap();
+    args
+});
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Listening: {}:{}", ARGS.addr, ARGS.port);
+    init_httpserver().await
 }
